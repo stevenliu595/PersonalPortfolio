f2c94f491dbe95472e9ecb777327adf0c8ef2543
stevenliu595 <sliu57@buffalo.edu>
2021-09-20 19:02:14 -0400

    commit

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function mobileMenu() {
   navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
+// const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
+// navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+// function closeMenu() {
+//   hamburger.classList.remove("active");
+//   navMenu.classList.remove("active");
+// }
